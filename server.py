eeed4d920068b89f7d4fbc73b39e4a5a3f74e106
Connor Rogstad <connornrogstad@gmail.com>
2025-04-15 13:57:12 -0700

    fixed spacing

--- server.py
+++ server.py
@@ -1,35 +1,28 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
-
-
 @app.route("/getcities/")
 def getCities():
-    return {"airpots": ["SBA", "SMX"]}
-
+    return {"airports": ["SBA", "SMX"]}
 
 @app.route("/temperature/<city>")
 def temperature(city):
     if city == "SBA":
-        return {"tempature": "72"}
+        return {"temperature": "72"}
     elif city == "SMX":
-        return {"tempature": "50"}
+        return {"temperature": "50"}
     else:
         return "Invalid Request"
 
 @app.route("/wind_speed/<city>")
 def wind_speed(city):
-    if city == "SBA": 
+    if city == "SBA":
         return {"speed": "2.2", "direction": "NW"}
-    elif city == "SMX": 
+    elif city == "SMX":
         return {"speed": "1.5", "direction": "S"}
     else:
         return "Invalid Request"
-
-
-
 
 @app.route("/cloud_cover/<city>")
 def cloud_cover(city):
@@ -39,3 +32,4 @@ def cloud_cover(city):
         return {"cloud_cover": "4"}
     else:
         return "Invalid Request"
+
