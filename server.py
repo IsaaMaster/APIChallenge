0b3e35067e0d0ebbacec5e3d060825f861289e63
Isaac Song <85504364+IsaaMaster@users.noreply.github.com>
2025-04-17 13:58:06 -0700

    Update server.py

--- server.py
+++ server.py
@@ -4,31 +4,31 @@ app = Flask(__name__)
 
 @app.route("/getcities/")
 def getCities():
-    return {"airports": ["SBA", "SMX"]}
+    return {"airports": ["sba", "smx"]}
 
 @app.route("/temperature/<city>")
 def temperature(city):
-    if city == "SBA":
+    if city == "sba":
         return {"temperature": "72"}
-    elif city == "SMX":
+    elif city == "smx":
         return {"temperature": "50"}
     else:
         return "Invalid Request"
 
 @app.route("/wind_speed/<city>")
 def wind_speed(city):
-    if city == "SBA":
+    if city == "sba":
         return {"speed": "2.2", "direction": "NW"}
-    elif city == "SMX":
+    elif city == "smx":
         return {"speed": "1.5", "direction": "S"}
     else:
         return "Invalid Request"
 
 @app.route("/cloud_cover/<city>")
 def cloud_cover(city):
-    if city == "SBA":
+    if city == "sba":
         return {"cloud_cover": "2"}
-    elif city == "SMX":
+    elif city == "smx":
         return {"cloud_cover": "4"}
     else:
         return "Invalid Request"
